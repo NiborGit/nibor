d8483fdfef39357cd0a421edd33b63d2c43096ce
Nibor <63475915+NiborGit@users.noreply.github.com>
2020-10-29 15:35:52 -0400

    Update biapp20es.js

--- js/biapp20es.js
+++ js/biapp20es.js
@@ -1,8 +1,5 @@
 var currIndex;
 var objChap;
-var fbb = 'c0e759b5-e9eb-426a-b842-fd5acb9e3318';
-var fbi = 'cb8ad352-2ab0-43af-9c8c-01f4930dc0c2';
-var fbc = '9d9232d8-c38a-4ff6-8cc4-47301f2ca553';
 showBooks();
 getChapters();
         var list = document.querySelector('div#myULbooks');
@@ -94,7 +91,7 @@ function getChapters(){
             console.log("chap loaded")
             }
           }; 
-      xmlhttp.open("GET", "https://firebasestorage.googleapis.com/v0/b/biblia-para-mi.appspot.com/o/BiNTChaptersEs.json?alt=media&token="+fbc, true);
+      xmlhttp.open("GET", "/nibor/Json/BiNTChaptersEs.json", true);
       xmlhttp.send();
 } 
     function clearAll(){
@@ -125,7 +122,7 @@ function getChapters(){
             });
            }
         };
-      xmlhttp.open("GET", "https://firebasestorage.googleapis.com/v0/b/biblia-para-mi.appspot.com/o/BiNTesBooks.json?alt=media&token="+fbi, true);
+      xmlhttp.open("GET", "/nibor/Json/BiNTesBooks.json", true);
       xmlhttp.send();
     }
     function showChapters(){          
@@ -172,7 +169,7 @@ function getChapters(){
             });
            }
         };
-      xmlhttp.open("GET", "https://firebasestorage.googleapis.com/v0/b/biblia-para-mi.appspot.com/o/BiNT_RV_es.json?alt=media&token="+fbb, true);
+      xmlhttp.open("GET", "/nibor/Json/BiNT_RV_es.json", true);
       xmlhttp.send();
     }
     function searchAll(){ 
